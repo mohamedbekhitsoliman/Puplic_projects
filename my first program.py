886d7068185413eed1bf7e46d3b620de95f2b47c
mohamedbekhitsoliman <114834176+mohamedbekhitsoliman@users.noreply.github.com>
2022-10-24 06:28:53 +0200

    upload file

---
+++ my first program.py
@@ -0,0 +1,113 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5+5
+10
+
+======= RESTART: D:/Data science/Programming paython/my first program.py =======
+hello world
+
+======= RESTART: D:/Data science/Programming paython/my first program.py =======
+hello world
+
+======= RESTART: D:/Data science/Programming paython/my first program.py =======
+hello world
+5/8
+
+======= RESTART: D:/Data science/Programming paython/my first program.py =======
+hello world
+5/8
+type (5)
+<class 'int'>
+type(3.33)
+<class 'float'>
+type(true)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    type(true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
+y
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    y
+NameError: name 'y' is not defined
+yes
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    yes
+NameError: name 'yes' is not defined
+
+======= RESTART: D:/Data science/Programming paython/my first program.py =======
+hello world
+5/8
+Traceback (most recent call last):
+  File "D:/Data science/Programming paython/my first program.py", line 5, in <module>
+    type(true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
+type (true)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    type (true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
+type (True)
+<class 'bool'>
+type(None)
+<class 'NoneType'>
+float(4)
+4.0
+#convert int   to   flOAT
+int (3.9)
+3
+# conver to int
+int(10/4)
+2
+10//4
+2
+#الغاء القيمة العشرية
+10%4
+2
+10%10
+0
+3%4
+3
+2**2
+4
+3**2
+9
+3**5
+243
+2**3
+8
+3*5+1
+16
+3+2*5
+13
+10/2+5
+10.0
+3*2/3+10-5
+7.0
+x=33
+x
+33
+x=x+2
+x
+35
+print ("x")
+x
+print x
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print 'x'
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print (x)
+35
+a=5
+A=6
+a+A
+11
+print(A*2)
+12
+print(a+9)
+print a
+print (a)
+
+user_name = "Marem"
+print (user_name)
